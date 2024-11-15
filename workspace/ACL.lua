b9d9ea7068c202ce93ad7fa9ba5c4b113f1b2918
1price <166103496+1price@users.noreply.github.com>
2024-11-15 23:31:52 +1100

    Update ACL.lua

--- workspace/ACL.lua
+++ workspace/ACL.lua
@@ -80,8 +80,8 @@ if TextChatService.ChatVersion == Enum.ChatVersion.TextChatService then
     end)
 
 else
-    if not pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/1price/usercreation/refs/heads/main/workspace/ACL"))() end) then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/1price/usercreation/refs/heads/main/workspace/ACL"))()
+    if not pcall(function() loadstring(game:HttpGet("https://raw.githubusercontent.com/1price/usercreation/refs/heads/main/workspace/ACL.lua"))() end) then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/1price/usercreation/refs/heads/main/workspace/ACL.lua"))()
     end
 end
 
