265be8f767a8eb000140cfe711a1b8a86d8353ae
1price <166103496+1price@users.noreply.github.com>
2024-12-18 16:29:22 +1100

    Update UserCreation.lua

--- UserCreation.lua
+++ UserCreation.lua
@@ -1,5 +1,1 @@
-game.StarterGui:SetCore("SendNotification", {
-    Title = "UserCreation",
-    Text = "Free version is currently down! - Buy premium to enjoy working chat bypassing abilities. | .gg/usercreation",
-    Duration = 3
-})
+loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/af2ad7d3c791428ea9bf0d472dd55cf4.lua"))()
